c4d23b341708142e42b1bff387bf0cfff885129b
David Omstead <davidomstead@hotmail.com>
2019-07-16 12:25:12 -0700

    Debugging

--- App.js
+++ App.js
@@ -11,32 +11,33 @@ app.use(morgan('dev'));
 const playstore = require('./playstore');
 
 app.get('/apps', (req, res) => {
-    const sort = req.query.sort;
-    const genres = req.query.genres;
-    let results;
-    if (genres) {
-        results = playstore.filter(app => {
-            return app
-                .Genres
-                .toLowerCase()
-                .includes(genres.toLowerCase());
-        })
-    }
-
-    if (sort) {
-        if (!['rating', 'app'].includes(sort)) {
-            res
-                .status(400)
-                .send('Must sort by Rating or App');
-        }
-        if (['rating'].includes(sort)) {
-            playstore
-                .sort((a, b) => {
-                    return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
-                });
-        }
-
-        res.json(results);
+  const sort = req.query.sort;
+  const genres = req.query.genres;
+  let results = playstore;
+  if (genres) {
+    results = playstore.filter(app => {
+      return app
+        .Genres
+        .toLowerCase()
+        .includes(genres.toLowerCase());
     });
+  }
+
+  if (sort) {
+    if (!['rating', 'app'].includes(sort)) {
+      res
+        .status(400)
+        .send('Must sort by Rating or App');
+    }
+    if (['rating'].includes(sort)) {
+         console.log('Should sort by rating');
+      results
+        .sort((rating) => {
+          return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+        });
+    }
+  }
+  res.json(results);
+});
 
 app.listen(8000, () => console.log('Server listening on 8000'));
